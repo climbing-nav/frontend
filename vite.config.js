994246037a4049f6cbc030fede018f81780125b9
sangmiiiiin <kwk680037@gmail.com>
2025-09-28 11:14:21 +0900

    🔐 HTTPS 설정 변경

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,10 @@ export default defineConfig({
     host: '0.0.0.0',  // 모든 네트워크 인터페이스에서 접근 허용
     port: 5173,
     strictPort: true,
-    // https: {
-    //   key: './ssl/vite-dev.key',
-    //   cert: './ssl/vite-dev.crt'
-    // },
+    https: {
+      key: './ssl/vite-dev.key',
+      cert: './ssl/vite-dev.crt'
+    },
     // 특정 호스트 명시적으로 허용
     allowedHosts: [
       'localhost',
